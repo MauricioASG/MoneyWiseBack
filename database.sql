9b9f65eceeae3b284a49bd5bb173dccea52c422e
Mauricio Alejandro Serrano García <mauricioalejandroserranogarcia@gmail.com>
2024-09-03 21:06:05 -0600

    The database was slightly modified to correct an error in the category table

--- database.sql
+++ database.sql
@@ -14,8 +14,9 @@ CREATE TABLE Usuario (
     fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+DROP TABLE IF EXISTS Categorias;
 CREATE TABLE Categorias (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     descripcion VARCHAR(255)
 );
@@ -36,8 +37,8 @@ CREATE TABLE Transacciones (
             'Regalos_Cumpleaños', 'Regalos_Navidad', 'Regalos_Otras ocasiones',
             'Ahorro e inversión_Ahorro a corto plazo', 'Ahorro e inversión_Ahorro a largo plazo', 'Ahorro e inversión_Inversiones',
             'Deudas_Tarjeta de crédito', 'Deudas_Préstamos personales', 'Deudas_Otros préstamos',
-            'Otros_Imprevistos', 'Otros_Donaciones', 'Otros_Impuestos (otros que no sean inmobiliarios)',
-            'Mascotas_Alimento', 'Mascotas_Veterinaria', 'Mascotas_Productos de higiene', 'Mascotas_Accesorios', 'Mascotas_Adiestramiento'
+            'Mascotas_Alimento', 'Mascotas_Veterinaria', 'Mascotas_Productos de higiene', 'Mascotas_Accesorios', 'Mascotas_Adiestramiento',
+            'Otros_Imprevistos', 'Otros_Donaciones', 'Otros_Impuestos (otros que no sean inmobiliarios)'
            ) NOT NULL,
   fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (usuario_id) REFERENCES Usuario(id),
@@ -45,24 +46,20 @@ CREATE TABLE Transacciones (
 );
 
 -- Inserciones en la tabla Categorias
-INSERT INTO Categorias (nombre, descripcion) 
-VALUES ('Vivienda', 'Gastos relacionados con la vivienda'),
-       ('Transporte', 'Gastos relacionados con el transporte'),
-       ('Alimentación', 'Gastos relacionados con la alimentación'),
-       ('Salud', 'Gastos relacionados con la salud'),
-       ('Educación', 'Gastos relacionados con la educación'),
-       ('Entretenimiento', 'Gastos relacionados con el entretenimiento'),
-       ('Ropa y calzado', 'Gastos relacionados con la ropa y el calzado'),
-       ('Regalos', 'Gastos relacionados con regalos'),
-       ('Ahorro e inversión', 'Gastos relacionados con el ahorro e inversión'),
-       ('Deudas', 'Gastos relacionados con las deudas'),
-       ('Otros', 'Otros gastos'),
-       ('Mascotas', 'Gastos relacionados con las mascotas');
+INSERT INTO Categorias (id, nombre, descripcion) 
+VALUES (0, 'Selecciona una categoría', 'Auxiliar para selección de categoría'),
+       (1, 'Vivienda', 'Gastos relacionados con la vivienda'),
+       (2, 'Transporte', 'Gastos relacionados con el transporte'),
+       (3, 'Alimentación', 'Gastos relacionados con la alimentación'),
+       (4, 'Salud', 'Gastos relacionados con la salud'),
+       (5, 'Educación', 'Gastos relacionados con la educación'),
+       (6, 'Entretenimiento', 'Gastos relacionados con el entretenimiento'),
+       (7, 'Ropa y calzado', 'Gastos relacionados con la ropa y el calzado'),
+       (8, 'Regalos', 'Gastos relacionados con regalos'),
+       (9, 'Ahorro e inversión', 'Gastos relacionados con el ahorro e inversión'),
+       (10, 'Deudas', 'Gastos relacionados con las deudas'),
+       (11, 'Mascotas', 'Gastos relacionados con las mascotas'),
+       (12, 'Otros', 'Otros gastos');
 
--- Actualizar la categoría auxiliar después
-UPDATE Categorias SET id = 0, nombre = 'Selecciona una categoría', descripcion = 'Auxiliar para selección de categoría' WHERE id = 1;
-
--- Ajustar el AUTO_INCREMENT
-ALTER TABLE Categorias AUTO_INCREMENT = 13;
-
-
+INSERT INTO Usuario (nombre, email, passw, salario)
+VALUES ('Mauricio Serrano', 'mau.com', '123', 40000.00);
